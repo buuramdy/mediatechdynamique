1e371743ec91d1c8eeb87fb5f0be88202696da0c
buuramdy <amdymoustaphandiaye@hotmail.fr>
2018-04-23 10:32:41 +0000

    my commit

---
+++ dropdown-master/menu.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <title>menu</title>
+        <link rel="stylesheet" href="css/bootstrap-cerulean.min.css"/>
+    </head>
+    <body>
+        <div class="nav navbar-inverse navbar-fixed-top " >
+            <div class="container-fluid">
+                <ul class="nav navbar-nav">
+                    <li><a href="?page=accueil"><strong>ACCEUIL</strong></a></li>
+                    <li class="dropdown">
+                        <a data-toggle="dropdown" href="#"><strong>FORUM 2018</strong><b
+                                class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li ><a href="?page=addclient">Thématique</a></li>
+                            <li><a href="?page=listeclient">Programme</a></li>
+                            <li ><a href="?page=addclient">Dossier De Participation</a></li>
+                            <li><a href="?page=listeclient">Internenants Ou Experts</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown">
+                        <a data-toggle="dropdown" href="#"><strong>PARTENAIRES</strong><b
+                                class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="?page=addfournisseur">Organisateurs</a></li>
+                            <li><a href="?page=listefournisseur">Partenaires Institutionnels</a></li>
+                            <li><a href="?page=addfournisseur">Partenaires Privés</a></li>
+                            <li><a href="?page=listefournisseur">Devenir Partenaire</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown">
+                        <a data-toggle="dropdown" href="#"><strong>MEDIATHEQUE</strong><b
+                                class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="?page=passercommande">Images</a></li>
+                            <li><a href="?page=consultercommande">Vidéos</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="?page=addfourniture"><strong>PRESSE</strong></a></li>
+                    
+                    <li class="dropdown">
+                        <a data-toggle="dropdown" href="#"><strong>NEWSLETTER</strong><b
+                                class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="?page=passercommande">S'Inscrire</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="?page=addfourniture"><strong>RAPPORT DE LA PREMIERE EDITION</strong></a></li>
+                    <li><a href="?page=addfourniture"><strong>SUIVEZ NOUS</strong></a></li>
+                </ul>
+                <div class="collapse navbar-collapse" id="navbar-collapse">
+                    <a href="menu.php" class="btn btn-info navbar-btn navbar-right"><strong>CONTACTEZ NOUS</strong></a>
+                </div>
+            </div>
+        </div>
+        <br><br><br>
+                <div class="collapse navbar-collapse" id="navbar-collapse">
+                    <a href="menu.php" class="btn btn-info navbar-btn navbar-right"><strong>FILM INSTITUTIONNEL DU FORUM</strong></a>
+                </div>
+        <script src="js/jquery.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <!--BACKSTRETCH-->
+        <!-- You can use an image of whatever size. This script will stretch to fit in any screen size.-->
+        <script type="text/javascript" src="js/jquery.backstretch.min.js"></script>
+        <script>
+            $.backstretch("img/f.jpg");
+        </script>
+    </body>
+</html>
+ 
+  
